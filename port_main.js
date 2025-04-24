12a995c45396726cb3e50b959c3f80f23c64fe0b
Soumen Mishra <mishrasoumen234@gmail.com>
2025-04-24 11:07:42 +0530

    intial changes

--- port_main.js
+++ port_main.js
@@ -31,7 +31,7 @@ var typingEffect = new Typed(".typedText",{
         reset: true     
  })
 
-
+// scsroll up and scroll down buttons 
  document.getElementById("scroll-toggle").addEventListener("click", function(event) {
     event.preventDefault();
 
@@ -50,3 +50,81 @@ var typingEffect = new Typed(".typedText",{
         scrollIcon.classList.add("uil-mouse-alt");
     }
 });
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+window.onscroll = function() {headerShadow()};
+
+function headerShadow() {
+  const navHeader =document.getElementById("header");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
+
+  } else {
+
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
+
+  }
+}
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const scrollRevealInstance= ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true     
+})
+
+/* -- HOME -- */
+scrollRevealInstance.reveal('.featured-text-card',{})
+scrollRevealInstance.reveal('.featured-name',{delay: 100})
+scrollRevealInstance.reveal('.featured-text-info',{delay: 200})
+scrollRevealInstance.reveal('.featured-text-btn',{delay: 200})
+scrollRevealInstance.reveal('.social_icons',{delay: 200})
+scrollRevealInstance.reveal('.featured-image',{delay: 300})
+
+
+/* -- PROJECT BOX -- */
+scrollRevealInstance.reveal('.project-box',{interval: 200})
+
+/* -- HEADINGS -- */
+scrollRevealInstance.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+origin: 'left',
+distance: '80px',
+duration: 2000,
+reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+origin: 'right',
+distance: '80px',
+duration: 2000,
+reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+
+
+
+
+
+
+
+
